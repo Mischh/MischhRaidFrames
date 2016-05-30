3be2978fb79e6c33165fbe0a01d5de2b4d338370
Mischh <mischhaffliction@gmail.com>
2016-05-30 23:59:02 +0200

    GroupHandler: Publish, Save&Load

--- MischhRaidFrames/GroupHandler.lua
+++ MischhRaidFrames/GroupHandler.lua
@@ -4,13 +4,20 @@ local GroupHandler = {}
 local MRF = Apollo.GetAddon("MischhRaidFrames")
 local groups = nil --applied in GetGroupHandlersRegroup
 local units = nil --applied in GetGroupHandlersRegroup
-
+local tinsert = table.insert
 
 local Options = MRF:GetOption(nil, "Group Handler")
+local optSavedDef = MRF:GetOption(Options, "cache")
+local optPermSave = MRF:GetOption(Options, "saved")
+local optUse = MRF:GetOption(Options, "use")
+local optAcc = MRF:GetOption(Options, "accept")
+local optLead = MRF:GetOption(Options, "lead")
 --MRF:AddMainTab("Group Handler", GroupHandler, "InitSettings")
 
-
+local accept = true --do we accept the leaders groupings?
+local onlyLead = true
 local useUserDef = false
+local permData = {}
 local userdef = {
 	--[1] = "Group1" --these Names NEED to be unique, the UI gets problems in other cases.
 	--[2] = "Group2" --this Group will end up empty -> not shown.
@@ -34,6 +41,15 @@ local function iwipe(tbl)
 		tbl[i] = nil
 	end
 end
+
+do
+	local f = MRF.OnDocLoaded
+	function MRF:OnDocLoaded(...)
+		f(self,...)
+		Apollo.RegisterEventHandler("ChatMessage", "OnChatMessage", GroupHandler)
+	end
+end
+
 
 function GroupHandler:Reposition() --overwritten in GetGroupHandlersRegroup
 end
@@ -83,6 +99,173 @@ function GroupHandler:Regroup_Default()
 	end
 end
 
+optPermSave:OnUpdate(GroupHandler, "NewPermanentSaves")
+function GroupHandler:NewPermanentSaves(new)
+	if type(new) ~= "table" then
+		optPermSave:Set({})
+	else
+		permData = new
+	end
+end
+
+optSavedDef:OnUpdate(GroupHandler, "NewSavedDef")
+function GroupHandler:NewSavedDef(new)
+	if type(new) ~= "table" then
+		optSavedDef:Set(userdef)
+	else
+		userdef = new
+		self:Regroup()
+		self:Reposition() 
+		self:DistantUpdate()
+	end
+end
+
+optLead:OnUpdate(GroupHandler, "UpdateRecieveLead")
+function GroupHandler:UpdateRecieveLead(new)
+	if new == nil then
+		optLead:Set(true)
+	else
+		onlyLead = new
+	end
+end
+
+function GroupHandler:VRFSerialize(t) --use VinceRaidFrames Serializing and Deserializing to be compatible.
+	local tbl = {"{"}
+	local indexed = #t > 0
+	local hasValues = false
+	for k, v in pairs(t) do
+		hasValues = true
+		tinsert(tbl, type(v) == "number" and tostring(v) or '"'..v..'"' )
+		tinsert(tbl, ",")
+	end
+	if hasValues then
+		tbl[#tbl] = nil
+	end
+	tinsert(tbl, "}")
+	return table.concat(tbl)
+end
+
+function GroupHandler:VRFDeserialize(str)
+	if type(str) ~= "string" then
+		return nil
+	end
+	local func = loadstring("return " .. str)
+	if not func then
+		return nil
+	end
+	setfenv(func, {})
+	local success, value = pcall(func)
+	return value
+end
+
+function GroupHandler:GetNameIdTable() --VinceRaidFrames (adapted)
+	local memberNames = {}
+	local memberNameToId = {}
+	for _, unit in ipairs(units) do
+		tinsert(memberNames, unit:GetName())
+	end
+	-- Sorted member names for short unique ids across clients!
+	table.sort(memberNames)
+	for i, name in ipairs(memberNames) do --this is just a array -> map method.
+		memberNameToId[name] = i
+	end
+	
+	return memberNameToId, memberNames
+end
+
+local shareKey = "}=>"
+function GroupHandler:PublishGroup()
+	local idTbl = self:GetNameIdTable()
+	
+	local groups = setmetatable({}, {__index = function(t,k) t[k] = {}; return t[k] end})
+	for i, v in pairs(userdef) do --group all members into their groups.
+		if type(i) == "number" then
+			groups[i][0] = v
+		else
+			tinsert(groups[v], i)
+		end
+	end
+	
+	local layout = {}
+	for _, grp in ipairs(groups) do --add the groups in the correct order into the layout - replace the names with IDs.
+		if grp[0] then --if the Group has a name, we will publish it.
+			tinsert(layout, grp[0])
+			for _, name in ipairs(grp) do
+				if idTbl[name] then --if the name does not have a ID -> scrap the name.
+					tinsert(layout, idTbl[name])
+				end
+			end
+		end
+	end
+	
+	local str = shareKey..self:VRFSerialize(layout)
+	ChatSystemLib.GetChannels()[ChatSystemLib.ChatChannel_Party]:Send(str)
+end
+
+-- tbl = {"GroupName1", idOfMemberInGroup1, "GroupName2", idOfMemberInGroup2, idOfMemberInGroup2, ...}
+function GroupHandler:ImportGroup(tbl) 
+	local _, idTbl = self:GetNameIdTable()
+	wipe(userdef)
+	
+	local grId = 0
+	
+	for _, v in ipairs(tbl) do
+		if type(v) == "string" then
+			grId = grId+1
+			userdef[grId] = v
+		else --number (aka ID)
+			local n = idTbl[v]
+			if n then
+				userdef[n] = grId
+			end
+		end
+	end
+	
+	self:Regroup()
+	self:Reposition()
+	self:DistantUpdate()
+end
+
+local matchString = "^"..shareKey.."(.+)$"
+function GroupHandler:OnChatMessage(channelSource, tMessageInfo)
+	if not accept then
+		return
+	end
+	if channelSource:GetType() ~= ChatSystemLib.ChatChannel_Party then
+		return
+	end
+	if onlyLead and not self:IsLead(tMessageInfo.strSender) then
+		return
+	end
+	local msg = {}
+	for i, segment in ipairs(tMessageInfo.arMessageSegments) do
+		tinsert(msg, segment.strText)
+	end
+	local strMsg = table.concat(msg, "")
+	strMsg = strMsg:match(matchString)
+	if not strMsg then
+		return
+	end
+	
+	local layout = self:VRFDeserialize(strMsg)
+	self:ImportGroup(layout)
+end
+
+function GroupHandler:IsLead(name)
+	for _, unit in ipairs(units) do
+		if unit:IsLeader() then
+			if unit:GetName() == name then
+				return true --is leader and has name.
+			else
+				return false --is leader, but hasnt name.
+			end
+		elseif unit:GetName() == name then
+			return false --isnt leader, but has name.
+		end
+	end
+	return false --no leader in group?! --nobody with the name?! --wtf?!
+end
+
 function MRF:GetGroupHandlersRegroup(unithandler_groups, unithandler_units, UnitHandler)
 	groups = unithandler_groups
 	units = unithandler_units
@@ -101,9 +284,11 @@ end
 --%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MRF: Grouping %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 local form = nil
 local grOptions = MRF:GetOption("Group Handler")
-local optUse = MRF:GetOption(grOptions, "useThis")
 local optGrName = MRF:GetOption(grOptions, "name")
 local optGrIdx = MRF:GetOption(grOptions, "index")
+local optSName = MRF:GetOption(grOptions, "saveName")
+local optLoad = MRF:GetOption(grOptions, "load")
+local optRemove = MRF:GetOption(grOptions, "remove")
 local handler = {}
 local ungrHandler = {} --ungrouped List		--ListItem creation within the Init!
 local grpdHandler = {} --grouped List		--ListItem creation within the Init!
@@ -111,8 +296,11 @@ local grouHandler = {} --groups List		--ListItem creation within the Init!
 local switching = false
 
 optUse:OnUpdate(handler, "UpdateUse")
+optAcc:OnUpdate(handler, "UpdateAccept")
 optGrName:OnUpdate(handler, "UpdateGrName")
 optGrIdx:OnUpdate(handler, "UpdateGroupIndex")
+optLoad:OnUpdate(handler, "LoadIndex")
+optRemove:OnUpdate(handler, "RemoveIndex")
 
 local classColor = unpack(MRF:HasModule("Class Colors"):GetColorTable())
 local name2color = {}
@@ -133,6 +321,8 @@ function handler:Update(onlyUI)
 end
 
 function GroupHandler:DistantUpdate()
+	if not units then return end
+	
 	name2color = {}
 	name2text = {}
 	for i, unit in ipairs(units) do
@@ -228,12 +418,28 @@ function handler:MoveFromGroup()
 	self:Update()
 end
 
+function handler:ToggleSaveMenu(btn)
+	self.saveMenu:Show(btn:IsChecked())
+end
+
+function handler:PublishGroup()
+	GroupHandler:PublishGroup()
+end
+
 function handler:UpdateUse(use)
 	if use == nil then
 		optUse:Set(false)
 	else
 		useUserDef = use
 		handler:Update()
+	end
+end
+
+function handler:UpdateAccept(acc)
+	if acc == nil then
+		optAcc:Set(true)
+	else
+		accept = acc
 	end
 end
 
@@ -263,6 +469,48 @@ function handler:UpdateGroupIndex(idx)
 	grouHandler:Redraw()
 	grpdHandler:Redraw()
 	switching = false
+end
+
+local function copy(tbl, target)
+	target = target or {}
+	for i,v in pairs(tbl) do
+		target[i] = v
+	end
+	return target
+end
+
+function handler:SaveThis(button)
+	local name = optSName:Get()
+	if not name or name:gsub("^%s+","") == "" then return end
+	
+	local idx = #permData+1
+	for i, t in ipairs(permData) do
+		if t.name == name then
+			idx = i
+			break;
+		end
+	end
+	
+	permData[idx] = permData[idx] or {["name"] = name}
+	permData[idx].data = copy(userdef)
+end
+
+function handler:LoadIndex(idx)
+	if idx and permData[idx] then
+		local name = permData[idx].name
+		wipe(userdef)
+		copy(permData[idx].data, userdef)
+		optSName:Set(name)
+		optLoad:Set(nil)
+		self:Update()
+	end
+end
+
+function handler:RemoveIndex(idx)
+	if idx and permData[idx] then
+		table.remove(permData, idx)
+		optRemove:Set(nil)
+	end
 end
 
 function grouHandler:GetText(idx)
@@ -371,13 +619,20 @@ function MRF:InitGroupForm()
 	if form then return form:Show(true, false) end
 	
 	form = self:LoadForm("GroupingForm", nil, handler)
+	handler.saveMenu = form:FindChild("SavesFrame")
 	ungrHandler.parent = form:FindChild("Ungrouped:Items")
 	grpdHandler.parent = form:FindChild("Grouped:Items")
 	grouHandler.parent = form:FindChild("Groups:Items")
 	
-	MRF:applyCheckbox(form:FindChild("Checkbox_Activated"), optUse, "Use")
+	MRF:applyCheckbox(form:FindChild("Checkbox_Activated"), optUse, "Activate").form:SetTooltip("Use these Settings instead of the Default tank-heal-dps groups.")
+	MRF:applyCheckbox(form:FindChild("Checkbox_Accept"), optAcc, "Accept").form:SetTooltip("Accept published Settings from Chat.")
+	MRF:applyCheckbox(form:FindChild("Checkbox_All"), optLead, "Lead only").form:SetTooltip("Published settings are only imported from the group-leader.")
 	MRF:applyTextbox(form:FindChild("Textbox_GroupName"), optGrName)
 	MRF:LoadForm("QuestionMark", form:FindChild("Textbox_GroupName")):SetTooltip([[Note: Every groups name needs to be unique.]])
+	
+	form:FindChild("Group_Publish"):SetTooltip([[Publishes your current settings to the group.
+	This and other addons may only accept the settings, if you are the leader of the group.
+	The addon can only publish settings for units, which are currently in your group. Make sure they are.]])
 	
 	ungrHandler = setmetatable(ungrHandler, {__index = function(t,k) 
 		if type(k) == "number" then
@@ -403,8 +658,28 @@ function MRF:InitGroupForm()
 			return item
 		end
 	end})
- 
 	
+	MRF:applyTextbox(form:FindChild("SavesFrame:Textbox_Name"),optSName)
+ 
+	local permRef = {
+		ipairs = function()
+			local tbl = {ipairs(permData)}
+			local f = tbl[1]
+			local function double(f,s,...) return f,f,... end
+			tbl[1] = function(...) return double(f(...)) end
+			return unpack(tbl)
+		end
+	}
+	local function trans(i)
+		return i and permData[i].name or "select one"
+	end
+	MRF:applyDropdown(form:FindChild("SavesFrame:Dropdown_Load"), permRef, optLoad, trans)
+	MRF:applyDropdown(form:FindChild("SavesFrame:Dropdown_Remove"), permRef, optRemove, trans)
+	
+	
+	
+	grOptions:ForceUpdate()
+	Options:ForceUpdate()
 	handler:Update()
 end
 
